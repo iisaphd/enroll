8df3f2845020599478bb26e536ddb5e3d978b61d
Battula <vrbattula@outlook.com>
2020-02-10 13:58:45 -0500

    Assign products to benefit sponsor catalog entity.

---
+++ components/benefit_markets/app/domain/benefit_markets/validators/products/premium_table_contract.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module BenefitMarkets
+  module Validators
+    module Products
+      class PremiumTableContract < Dry::Validation::Contract
+
+        params do
+          required(:effective_period).filled(type?: Range)
+          required(:rating_area).filled(:hash)
+          optional(:premium_tuples).maybe(:hash)
+        end
+
+        rule(:rating_area) do
+          if key? && value
+            result = Locations::RatingAreaContract.new.call(value)
+            key.failure(text: "invalid rating area", error: result.errors.to_h) if result&.failure?
+          end
+        end
+
+        rule(:premium_tuples).each do
+          if key? && value
+            result = PremiumTupleContract.new.call(value)
+            key.failure(text: "invalid premium tuple", error: result.errors.to_h) if result&.failure?
+          end
+        end
+      end
+    end
+  end
+end
