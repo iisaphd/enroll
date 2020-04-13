4a5ea9e67d87c58d010f2a43e16ff2f91101ee2b
harshaellanki <h_vardhan@yahoo.com>
2020-04-13 10:23:30 -0400

    removed omittable: true and updated specs

---
+++ components/benefit_markets/app/domain/benefit_markets/entities/member_relationship.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module BenefitMarkets
+  module Entities
+    class MemberRelationship < Dry::Struct
+      transform_keys(&:to_sym)
+
+      attribute :relationship_name,         Types::Strict::Symbol
+      attribute :relationship_kinds,        Types::Strict::Array
+
+      attribute :age_threshold,             Types::Integer.optional
+      attribute :age_comparison,            Types::Symbol.optional
+      attribute :disability_qualifier,      Types::Bool.optional
+    end
+  end
+end
