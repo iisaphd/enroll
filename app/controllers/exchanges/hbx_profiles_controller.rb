9e5ea32cb1d5519a1c6ff498b41f47d5daf57b90
hithesh <hitheshk07@gmail.com>
2020-12-23 16:32:04 -0500

    brakeman weak fixes

--- app/controllers/exchanges/hbx_profiles_controller.rb
+++ app/controllers/exchanges/hbx_profiles_controller.rb
@@ -117,7 +117,7 @@ class Exchanges::HbxProfilesController < ApplicationController
     @orgs = Organization.search(@q).exists(employer_profile: true)
     @page_alphabets = page_alphabets(@orgs, "legal_name")
     page_no = cur_page_no(@page_alphabets.first)
-    @organizations = @orgs.where("legal_name" => /^#{page_no}/i)
+    @organizations = @orgs.where("legal_name" => /^#{Regexp.escape(page_no)}/i) if page_no.present?
 
     @employer_profiles = @organizations.map {|o| o.employer_profile}
 
@@ -212,7 +212,7 @@ def employer_poc
     @page_alphabets = page_alphabets(@staff, "last_name")
     page_no = cur_page_no(@page_alphabets.first)
     if @q.nil?
-      @staff = @staff.where(last_name: /^#{page_no}/i)
+      @staff = @staff.where(last_name: /^#{Regexp.escape(page_no)}/i)
     else
       @staff = @staff.where(last_name: @q)
     end
@@ -224,7 +224,7 @@ def employer_poc
     @page_alphabets = page_alphabets(@staff, "last_name")
     page_no = cur_page_no(@page_alphabets.first)
     if @q.nil?
-      @staff = @staff.where(last_name: /^#{page_no}/i)
+      @staff = @staff.where(last_name: /^#{Regexp.escape(page_no)}/i)
     else
       @staff = @staff.where(last_name: @q)
     end
