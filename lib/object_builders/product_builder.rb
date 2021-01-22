0ed4e961d4a8ed08b3f57938ccd6bbae1eb28590
vineel <sonnathivinil@gmail.com>
2021-01-22 12:39:58 -0500

    Fix specs

--- lib/object_builders/product_builder.rb
+++ lib/object_builders/product_builder.rb
@@ -210,7 +210,7 @@ class ProductBuilder
 
   def build_deductible
     plan_deductible_list_params.each do |plan_deductible|
-      @csv.qhp_deductables.build(plan_deductible)
+      @csv.qhp_deductibles.build(plan_deductible)
     end
   end
 
