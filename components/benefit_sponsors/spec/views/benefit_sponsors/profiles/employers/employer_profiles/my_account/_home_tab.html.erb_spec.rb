ca4aa8c1c7af75d7b72acfd2983a6dc9c2ebe7a9
Battula <vrbattula@outlook.com>
2021-05-11 19:56:13 -0400

    refs #R4 Fixes spec failures

--- components/benefit_sponsors/spec/views/benefit_sponsors/profiles/employers/employer_profiles/my_account/_home_tab.html.erb_spec.rb
+++ components/benefit_sponsors/spec/views/benefit_sponsors/profiles/employers/employer_profiles/my_account/_home_tab.html.erb_spec.rb
@@ -188,6 +188,7 @@ RSpec.describe "components/benefit_sponsors/app/views/benefit_sponsors/profiles/
         benefit_groups: benefit_groups,
         aasm_state: 'draft',
         predecessor_id: nil,
+        employee_participation_ratio_minimum: 0.75,
         employer_profile: double(census_employees: double(active: active_employees))
       )
     end
