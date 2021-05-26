2ac255197e1e5df63162a9ec6cb082e0a98f0968
Zane Sandin <zanesandin@gmail.com>
2021-05-26 18:10:17 -0400

    REFS maintenance - adds presence checker.

--- app/models/group_selection_eligibility_checker.rb
+++ app/models/group_selection_eligibility_checker.rb
@@ -6,9 +6,9 @@ class GroupSelectionEligibilityChecker
   MemberAgeSlug = Struct.new(:dob, :member_id)
 
   def initialize(benefit_package, coverage_kind)
-    @sponsored_benefit = benefit_package&.sponsored_benefits.detect do |sb|
-      sb&.product_kind&.to_s == coverage_kind.to_s
-    end
+    @sponsored_benefit = benefit_package.sponsored_benefits.detect do |sb|
+      sb.product_kind.to_s == coverage_kind.to_s
+    end if benefit_package.present?
     @contribution_model = @sponsored_benefit.contribution_model if @sponsored_benefit.present?
     @age_calculator = ::BenefitSponsors::CoverageAgeCalculator.new
   end
