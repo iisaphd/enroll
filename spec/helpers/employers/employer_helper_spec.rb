naort-dev/enroll-ruby

refs R4 Fix employer helper spec

spec/helpers/employers/employer_helper_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Employers::EmployerHelper, :type => :helper, dbclean: :after_each
     let(:benefit_sponsorship) {FactoryBot.create(:benefit_sponsors_benefit_sponsorship, :with_benefit_market, :with_organization_cca_profile, :with_initial_benefit_application)}
     let(:benefit_package) {benefit_sponsorship.benefit_applications.first.benefit_packages.first}
     let(:census_employee) {FactoryBot.build(:benefit_sponsors_census_employee, employer_profile: benefit_sponsorship.profile, benefit_sponsorship: benefit_sponsorship)}
-    let(:benefit_group_assignment) {FactoryBot.build(:benefit_group_assignment, benefit_package_id: benefit_package.id, census_employee: census_employee)}
+    let(:benefit_group_assignment) {FactoryBot.create(:benefit_group_assignment, benefit_package_id: benefit_package.id, census_employee: census_employee, start_on: benefit_package.start_on, end_on: benefit_package.end_on)}
     let(:health_plan) {FactoryBot.create(:plan, coverage_kind: "health")}
     let(:dental_plan) {FactoryBot.create(:plan, coverage_kind: "dental", dental_level: "high")}
     let(:health_enrollment) {FactoryBot.create(:hbx_enrollment,

iisaphd/enroll

spec/helpers/employers/employer_helper_spec.rb
@@ -9,10 +9,11 @@ RSpec.describe Employers::EmployerHelper, :type => :helper, dbclean: :after_each
     let(:benefit_sponsorship) {FactoryGirl.create(:benefit_sponsors_benefit_sponsorship, :with_benefit_market, :with_organization_cca_profile, :with_initial_benefit_application)}
     let(:benefit_package) {benefit_sponsorship.benefit_applications.first.benefit_packages.first}
     let(:census_employee) {FactoryGirl.build(:benefit_sponsors_census_employee, employer_profile: benefit_sponsorship.profile, benefit_sponsorship: benefit_sponsorship)}
-    let(:benefit_group_assignment) {FactoryGirl.build(:benefit_group_assignment, benefit_package_id: benefit_package.id, census_employee: census_employee)}
+    let(:benefit_group_assignment) {FactoryGirl.create(:benefit_group_assignment, benefit_package_id: benefit_package.id, census_employee: census_employee, start_on: benefit_package.start_on, end_on: benefit_package.end_on)}
     let(:health_plan) {FactoryGirl.create(:plan, coverage_kind: "health")}
     let(:dental_plan) {FactoryGirl.create(:plan, coverage_kind: "dental", dental_level: "high")}
     let(:health_enrollment) {FactoryGirl.create(:hbx_enrollment,
+                                                family: primary_family,
                                                 household: primary_family.latest_household,
                                                 employee_role_id: employee_role.id,
                                                 coverage_kind: "health",