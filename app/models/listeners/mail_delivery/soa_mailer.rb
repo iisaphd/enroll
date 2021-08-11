1c1b1b25e5d07dcc3ae2d3f420b8ef3bfcff3abf
Zane Sandin <zanesandin@gmail.com>
2021-08-11 15:38:59 -0400

    REFS Maintenance - Transliterates any special latin accent marks in soa mailer.
    Stuff
    
    Update soa_mailer.rb
    
    Update soa_mailer.rb
    
    REFS maintenance removes asci commnents.

--- app/models/listeners/mail_delivery/soa_mailer.rb
+++ app/models/listeners/mail_delivery/soa_mailer.rb
@@ -7,9 +7,14 @@ module MailDelivery
     end
 
     def deliver!(mail)
-      subject = mail.subject
-      body = mail.body.raw_source
       mail.to.each do |recipient|
+        # https://stackoverflow.com/a/20586777/5331859
+        # Will transliterate any special latin accent marks
+        # into regular latin characters
+        # I18n.transliterate("Text with speceial accent marks")
+        # Converts to a string without accent marks
+        # => "Instrucciones de recuperacion de contrasena"
+        subject = I18n.transliterate(subject)
         send_email_html(recipient, subject, body)
       end
     end
